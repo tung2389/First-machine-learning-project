a0361524ba50464a2b5b1b27e6d3e2e87d165a8e
Tung Luu <luukhanhtung2002@gmail.com>
2019-07-10 09:44:35 +0700

    Reorganize files and folders and jump into KNN algorithm

---
+++ KNN/KNN.py
@@ -0,0 +1,11 @@
+import sklearn
+from sklearn.utils import shuffle
+from sklearn.neighbors import KNeighborsClassifier
+import pandas as pd
+import numpy as np
+from sklearn import linear_model, preprocessing
+# from  import createValidPath
+
+path = createValidPath("car.data")
+data = pd.read_csv(path)
+print(data.head())
--- linearRegression/linearRegression.py
+++ linearRegression/linearRegression.py
@@ -3,10 +3,19 @@ import numpy as np
 import sklearn
 from sklearn import linear_model
 from sklearn.utils import shuffle
+import matplotlib.pyplot as pyplot
+import pickle
+from matplotlib import style
+import os
+
+def createValidPath(filename):
+     path = os.path.join(os.path.dirname(__file__), filename)
+     return path
 
 def readAndHandleData():
     global data, simplified_data, predict
-    data = pd.read_csv("student-mat.csv", sep=";")
+    path = createValidPath("student-mat.csv")
+    data = pd.read_csv(path, sep=";")
     simplified_data = data[{"G1","G2","G3","studytime","failures","absences"}]
     predict = "G3"
 
@@ -34,10 +43,42 @@ def printAllPredictionsAndActualValues():
     for x in range(len(predictions)):
         print("Predictions: ", predictions[x], "Inputs: ", x_test[x], "Actual outputs: ", y_test[x])
 
+def saveTheModel():
+     path = createValidPath("studentmodel.pickle")
+     with open(path, "wb") as f:
+          pickle.dump(linear, f)
+
+def loadTheBestModel():
+     path = createValidPath("studentmodel.pickle")
+     pickle_in = open(path, "rb")
+     global linear
+     linear = pickle.load(pickle_in)
+
+def findTheBestModel():
+     best = 0
+     for _ in range(40):
+        trainTheModel()
+        accuracy_rate = linear.score(x_test, y_test)
+        if(accuracy_rate > best):
+             best = accuracy_rate
+             printAccuracyRate()
+             printMoreInfo()
+             saveTheModel()
+
+def plotTheData(x, y):
+     style.use("ggplot")
+     pyplot.scatter(data[x], data[y])
+     pyplot.xlabel(x)
+     pyplot.ylabel(y)
+     pyplot.show()   
 
 readAndHandleData()
 getFeaturesAndLabel()
-trainTheModel()
-printAccuracyRate()
+# findTheBestModel()
+loadTheBestModel()
+# printAccuracyRate()
 printMoreInfo()
-printAllPredictionsAndActualValues()
+# printAllPredictionsAndActualValues()
+plotTheData("studytime", predict)
+
+
