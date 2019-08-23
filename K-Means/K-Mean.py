faed10a16889a58a2081e857dd04b6ffaa5891df
Tung Luu <luukhanhtung2002@gmail.com>
2019-08-23 23:20:33 +0700

    Complete the tutorial. Feel a bit confusing

---
+++ K-Means/K-Mean.py
@@ -0,0 +1,28 @@
+import numpy as np
+import sklearn
+from sklearn.preprocessing import scale
+from sklearn.datasets import load_digits
+from sklearn.cluster import KMeans
+from sklearn import metrics
+
+digits = load_digits()
+data = scale(digits.data) #Scale the data down between -1 and 1
+y = digits.target
+
+centroids_number = 10
+samples, features = data.shape
+
+def bench_k_means(estimator, name, data):
+    estimator.fit(data)
+    print('%-9s\t%i\t%.3f\t%.3f\t%.3f\t%.3f\t%.3f\t%.3f'
+          % (name, estimator.inertia_,
+             metrics.homogeneity_score(y, estimator.labels_),
+             metrics.completeness_score(y, estimator.labels_),
+             metrics.v_measure_score(y, estimator.labels_),
+             metrics.adjusted_rand_score(y, estimator.labels_),
+             metrics.adjusted_mutual_info_score(y,  estimator.labels_),
+             metrics.silhouette_score(data, estimator.labels_,
+                                      metric='euclidean')))
+
+classifer = KMeans(n_clusters=centroids_number, init="random", n_init=10)
+bench_k_means(classifer, "1", data)
